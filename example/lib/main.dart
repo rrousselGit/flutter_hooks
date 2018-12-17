871f80a2013a4434e84778a778d2f6ada4f28e59
Remi Rousselet <darky12s@gmail.com>
2018-12-18 00:46:44 +0100

    pub initial publish

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_hooks/hook.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 
 void main() => runApp(_MyApp());
 
--- test/mock.dart
+++ test/mock.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/widgets.dart';
-import 'package:flutter_hooks/hook.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
