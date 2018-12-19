259961a23623eeb9071cca2f3b7f0bc9c8c50cb8
Remi Rousselet <darky12s@gmail.com>
2018-12-19 16:45:35 +0100

    fix coverage bug

--- test/mock.dart
+++ test/mock.dart
@@ -6,17 +6,6 @@ import 'package:mockito/mockito.dart';
 export 'package:flutter_test/flutter_test.dart' hide Func0, Func1;
 export 'package:mockito/mockito.dart';
 
-class MockHook<R> extends Hook<R> {
-  final MockHookState<R> state;
-
-  MockHook([MockHookState<R> state]) : state = state ?? MockHookState();
-
-  @override
-  MockHookState<R> createState() => state;
-}
-
-class MockHookState<R> extends Mock implements HookState<R, MockHook<R>> {}
-
 abstract class _Func0<R> {
   R call();
 }
