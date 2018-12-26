ed2d57d7d67af818964daeed8a6a5eacb503b9fb
Remi Rousselet <darky12s@gmail.com>
2018-12-26 19:06:40 +0100

    Edit changelog

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -22,6 +22,8 @@ class _Counter extends HookWidget {
 
   @override
   Widget build(HookContext context) {
+    StreamController<int> countController =
+        _useLocalStorageInt(context, 'counter');
     return Scaffold(
       appBar: AppBar(
         title: const Text('Counter app'),
@@ -29,24 +31,15 @@ class _Counter extends HookWidget {
       body: Center(
         child: HookBuilder(
           builder: (context) {
-            // TODO: moving this to the root, and removing foo throws an assertion fail
-            StreamController<int> countController =
-                _useLocalStorageInt(context, 'counter');
             AsyncSnapshot<int> count =
                 context.useStream(countController.stream);
 
-            final foo = context.useMemoized(() {
-              return count.data;
-            }, <dynamic>[(count.data ?? 0) % 4 == 0]);
-
-            print('${foo ?? -1}  ${(count.data ?? 0) / 4} ');
             return !count.hasData
                 // Currently loading value from local storage, or there's an error
                 ? const CircularProgressIndicator()
                 : GestureDetector(
                     onTap: () => countController.add(count.data + 1),
-                    child: Text(
-                        'You tapped me ${count.data} times memoized $foo '),
+                    child: Text('You tapped me ${count.data} times.'),
                   );
           },
         ),
