9b751cdb0af3b888368dc94a6b0d5328b11f4233
Remi Rousselet <darky12s@gmail.com>
2018-12-28 12:27:46 +0100

    useReducer (#23)

--- lib/src/hooks.dart
+++ lib/src/hooks.dart
@@ -5,6 +5,112 @@ import 'package:flutter/scheduler.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/src/framework.dart';
 
+/// A [HookWidget] that defer its [HookWidget.build] to a callback
+class HookBuilder extends HookWidget {
+  /// The callback used by [HookBuilder] to create a widget.
+  ///
+  /// If a [Hook] asks for a rebuild, [builder] will be called again.
+  /// [builder] must not return `null`.
+  final Widget Function(BuildContext context) builder;
+
+  /// Creates a widget that delegates its build to a callback.
+  ///
+  /// The [builder] argument must not be null.
+  const HookBuilder({
+    @required this.builder,
+    Key key,
+  })  : assert(builder != null),
+        super(key: key);
+
+  @override
+  Widget build(BuildContext context) => builder(context);
+}
+
+/// A state holder that allows mutations by dispatching actions.
+abstract class Store<State, Action> {
+  /// The current state.
+  ///
+  /// This value may change after a call to [dispatch].
+  State get state;
+
+  /// Dispatches an action.
+  ///
+  /// Actions are dispatched synchronously.
+  /// It is impossible to try to dispatch actions during [HookWidget.build].
+  void dispatch(Action action);
+}
+
+/// Composes an [Action] and a [State] to create a new [State].
+///
+/// [Reducer] must never return `null`, even if [state] or [action] are `null`.
+typedef Reducer<State, Action> = State Function(State state, Action action);
+
+/// An alternative to [useState] for more complex states.
+///
+/// [useReducer] manages an read only state that can be updated
+/// by dispatching actions which are interpreted by a [Reducer].
+///
+/// [reducer] is immediatly called on first build with [initialAction]
+/// and [initialState] as parameter.
+///
+/// It is possible to change the [reducer] by calling [useReducer]
+///  with a new [Reducer].
+///
+/// See also:
+///  * [Reducer]
+///  * [Store]
+Store<State, Action> useReducer<State extends Object, Action>(
+  Reducer<State, Action> reducer, {
+  State initialState,
+  Action initialAction,
+}) {
+  return Hook.use(_ReducerdHook(reducer,
+      initialAction: initialAction, initialState: initialState));
+}
+
+class _ReducerdHook<State, Action> extends Hook<Store<State, Action>> {
+  final Reducer<State, Action> reducer;
+  final State initialState;
+  final Action initialAction;
+
+  const _ReducerdHook(this.reducer, {this.initialState, this.initialAction})
+      : assert(reducer != null);
+
+  @override
+  _ReducerdHookState<State, Action> createState() =>
+      _ReducerdHookState<State, Action>();
+}
+
+class _ReducerdHookState<State, Action>
+    extends HookState<Store<State, Action>, _ReducerdHook<State, Action>>
+    implements Store<State, Action> {
+  @override
+  State state;
+
+  @override
+  void initHook() {
+    super.initHook();
+    state = hook.reducer(hook.initialState, hook.initialAction);
+    assert(state != null);
+  }
+
+  @override
+  void dispatch(Action action) {
+    final res = hook.reducer(state, action);
+    assert(res != null);
+    if (state != res) {
+      setState(() {
+        state = res;
+      });
+    }
+  }
+
+  @override
+  Store<State, Action> build(BuildContext context) {
+    return this;
+  }
+}
+
 /// Create and cache the instance of an object.
 ///
 /// [useMemoized] will immediatly call [valueBuilder] on first call and store its result.
