2777ec99a8b862f0d1b7989527ecddf10ef2d95c
Remi Rousselet <darky12s@gmail.com>
2018-12-28 12:19:19 +0100

    useReducer

--- lib/src/framework.dart
+++ lib/src/framework.dart
@@ -427,24 +427,3 @@ class _HookWidgetState extends State<HookWidget> {
     return widget.build(context);
   }
 }
-
-/// A [HookWidget] that defer its [HookWidget.build] to a callback
-class HookBuilder extends HookWidget {
-  /// The callback used by [HookBuilder] to create a widget.
-  ///
-  /// If a [Hook] asks for a rebuild, [builder] will be called again.
-  /// [builder] must not return `null`.
-  final Widget Function(BuildContext context) builder;
-
-  /// Creates a widget that delegates its build to a callback.
-  ///
-  /// The [builder] argument must not be null.
-  const HookBuilder({
-    @required this.builder,
-    Key key,
-  })  : assert(builder != null),
-        super(key: key);
-
-  @override
-  Widget build(BuildContext context) => builder(context);
-}
