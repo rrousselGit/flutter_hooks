2b4f40b9f1ad5e7dcc3173adecfb3eecf5e2a446
Sahand Akbarzadeh <sahandevs@gmail.com>
2019-10-08 13:16:09 +0330

    implement useReassemble (#103)
    resolves #60

---
+++ test/use_reassemble_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
+
+import 'mock.dart';
+
+void main() {
+  testWidgets('useReassemble null callback throws', (tester) async {
+    await expectPump(
+      () => tester.pumpWidget(HookBuilder(builder: (c) {
+        useReassemble(null);
+        return Container();
+      })),
+      throwsAssertionError,
+    );
+  });
+
+  testWidgets('hot-reload calls useReassemble\'s callback', (tester) async {
+    final reassemble = Func0<void>();
+    await tester.pumpWidget(HookBuilder(builder: (context) {
+      useReassemble(reassemble);
+      return Container();
+    }));
+
+    verifyNoMoreInteractions(reassemble);
+
+    hotReload(tester);
+    await tester.pump();
+
+    verify(reassemble()).called(1);
+    verifyNoMoreInteractions(reassemble);
+  });
+
+}
