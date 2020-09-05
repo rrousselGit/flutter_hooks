dd83dcda974e96d215b8779ce21d0115877f4548
Remi Rousselet <darky12s@gmail.com>
2020-09-05 18:12:57 +0200

    Support parameter update & add more tests for FocusNode

--- test/use_focus_node_test.dart
+++ test/use_focus_node_test.dart
@@ -4,7 +4,7 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'mock.dart';
 
 void main() {
-  testWidgets('creates a focus node', (tester) async {
+  testWidgets('creates a focus node and disposes it', (tester) async {
     FocusNode focusNode;
     await tester.pumpWidget(
       HookBuilder(builder: (_) {
@@ -37,5 +37,82 @@ void main() {
       () => focusNode.hasListeners,
       throwsAssertionError,
     );
+  });
+
+  testWidgets('default values matches with FocusNode', (tester) async {
+    final official = FocusNode();
+
+    FocusNode focusNode;
+    await tester.pumpWidget(
+      HookBuilder(builder: (_) {
+        focusNode = useFocusNode();
+        return Container();
+      }),
+    );
+
+    expect(focusNode.debugLabel, official.debugLabel);
+    expect(focusNode.onKey, official.onKey);
+    expect(focusNode.skipTraversal, official.skipTraversal);
+    expect(focusNode.canRequestFocus, official.canRequestFocus);
+    expect(focusNode.descendantsAreFocusable, official.descendantsAreFocusable);
+  });
+
+  testWidgets('has all the FocusNode parameters', (tester) async {
+    bool onKey(FocusNode node, RawKeyEvent event) => true;
+
+    FocusNode focusNode;
+    await tester.pumpWidget(
+      HookBuilder(builder: (_) {
+        focusNode = useFocusNode(
+          debugLabel: 'Foo',
+          onKey: onKey,
+          skipTraversal: true,
+          canRequestFocus: false,
+          descendantsAreFocusable: false,
+        );
+        return Container();
+      }),
+    );
+
+    expect(focusNode.debugLabel, 'Foo');
+    expect(focusNode.onKey, onKey);
+    expect(focusNode.skipTraversal, true);
+    expect(focusNode.canRequestFocus, false);
+    expect(focusNode.descendantsAreFocusable, false);
+  });
+
+  testWidgets('handles parameter change', (tester) async {
+    bool onKey(FocusNode node, RawKeyEvent event) => true;
+    bool onKey2(FocusNode node, RawKeyEvent event) => true;
+
+    FocusNode focusNode;
+    await tester.pumpWidget(
+      HookBuilder(builder: (_) {
+        focusNode = useFocusNode(
+          debugLabel: 'Foo',
+          onKey: onKey,
+          skipTraversal: true,
+          canRequestFocus: false,
+          descendantsAreFocusable: false,
+        );
+        return Container();
+      }),
+    );
+
+    await tester.pumpWidget(
+      HookBuilder(builder: (_) {
+        focusNode = useFocusNode(
+          debugLabel: 'Bar',
+          onKey: onKey2,
+        );
+        return Container();
+      }),
+    );
+
+    expect(focusNode.onKey, onKey, reason: 'onKey has no setter');
+    expect(focusNode.debugLabel, 'Bar');
+    expect(focusNode.skipTraversal, false);
+    expect(focusNode.canRequestFocus, true);
+    expect(focusNode.descendantsAreFocusable, true);
   });
 }
