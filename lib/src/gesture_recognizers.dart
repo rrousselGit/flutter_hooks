3f7f8b21f74c549f7010fd3862e8d2814a6919a6
Albert221 <w.albert221@gmail.com>
2020-10-03 16:00:26 +0200

    Add generic GestureRecognizer and TapGestureRecognizer hook

---
+++ lib/src/gesture_recognizers.dart
@@ -0,0 +1,55 @@
+part of 'hooks.dart';
+
+abstract class _GestureRecognizerHook<G extends GestureRecognizer>
+    extends Hook<G> {
+  const _GestureRecognizerHook(List<Object> keys) : super(keys: keys);
+
+  @override
+  HookState<G, Hook<G>> createState() => _GestureRecognizerHookState<G>();
+
+  G initRecognizer();
+}
+
+class _GestureRecognizerHookState<G extends GestureRecognizer>
+    extends HookState<G, _GestureRecognizerHook<G>> {
+  G recognizer;
+
+  @override
+  void initHook() => recognizer = hook.initRecognizer();
+
+  @override
+  G build(BuildContext context) => recognizer;
+
+  @override
+  void dispose() => recognizer?.dispose();
+
+  @override
+  String get debugLabel => 'use${G.runtimeType}';
+}
+
+/// Creates and disposes a [TapGestureRecognizer].
+///
+/// See also:
+/// - [TapGestureRecognizer]
+TapGestureRecognizer useTapGestureRecognizer({
+  Object debugOwner,
+  List<Object> keys,
+}) =>
+    use(_TapGestureRecognizerHook(
+      debugOwner: debugOwner,
+      keys: keys,
+    ));
+
+class _TapGestureRecognizerHook
+    extends _GestureRecognizerHook<TapGestureRecognizer> {
+  const _TapGestureRecognizerHook({
+    this.debugOwner,
+    List<Object> keys,
+  }) : super(keys);
+
+  final Object debugOwner;
+
+  @override
+  TapGestureRecognizer initRecognizer() =>
+      TapGestureRecognizer(debugOwner: debugOwner);
+}
--- lib/src/hooks.dart
+++ lib/src/hooks.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart' show TabController;
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/widgets.dart';
@@ -9,10 +10,11 @@ import 'framework.dart';
 
 part 'animation.dart';
 part 'async.dart';
+part 'focus.dart';
+part 'gesture_recognizers.dart';
 part 'listenable.dart';
 part 'misc.dart';
 part 'primitives.dart';
+part 'scroll_controller.dart';
 part 'tab_controller.dart';
 part 'text_controller.dart';
-part 'focus.dart';
-part 'scroll_controller.dart';
