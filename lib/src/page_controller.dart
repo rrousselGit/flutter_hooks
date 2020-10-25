43f450e08a15155ee9bd454a5d41dc5617a49b26
HANAI tohru <tohru@reedom.com>
2020-10-25 23:09:01 +0900

    add usePageController
    based on useScrollController implementation.

---
+++ lib/src/page_controller.dart
@@ -0,0 +1,61 @@
+part of 'hooks.dart';
+
+/// Creates and disposes a [PageController].
+///
+/// See also:
+/// - [PageController]
+PageController usePageController({
+  int initialPage = 0,
+  bool keepPage = true,
+  double viewportFraction = 1.0,
+  List<Object> keys,
+}) {
+  return use(
+    _PageControllerHook(
+      initialPage: initialPage,
+      keepPage: keepPage,
+      viewportFraction: viewportFraction,
+      keys: keys,
+    ),
+  );
+}
+
+class _PageControllerHook extends Hook<PageController> {
+  const _PageControllerHook({
+    this.initialPage,
+    this.keepPage,
+    this.viewportFraction,
+    List<Object> keys,
+  }) : super(keys: keys);
+
+  final int initialPage;
+  final bool keepPage;
+  final double viewportFraction;
+
+  @override
+  HookState<PageController, Hook<PageController>> createState() =>
+      _PageControllerHookState();
+}
+
+class _PageControllerHookState
+    extends HookState<PageController, _PageControllerHook> {
+  PageController controller;
+
+  @override
+  void initHook() {
+    controller = PageController(
+      initialPage: hook.initialPage,
+      keepPage: hook.keepPage,
+      viewportFraction: hook.viewportFraction,
+    );
+  }
+
+  @override
+  PageController build(BuildContext context) => controller;
+
+  @override
+  void dispose() => controller.dispose();
+
+  @override
+  String get debugLabel => 'usePageController';
+}
---
+++ test/use_page_controller_test.dart
@@ -0,0 +1,99 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:flutter_hooks/src/framework.dart';
+import 'package:flutter_hooks/src/hooks.dart';
+
+import 'mock.dart';
+
+void main() {
+  testWidgets('debugFillProperties', (tester) async {
+    await tester.pumpWidget(
+      HookBuilder(builder: (context) {
+        usePageController();
+        return const SizedBox();
+      }),
+    );
+
+    final element = tester.element(find.byType(HookBuilder));
+
+    expect(
+      element
+          .toDiagnosticsNode(style: DiagnosticsTreeStyle.offstage)
+          .toStringDeep(),
+      equalsIgnoringHashCodes(
+        'HookBuilder\n'
+        ' │ usePageController: PageController#00000(no clients)\n'
+        ' └SizedBox(renderObject: RenderConstrainedBox#00000)\n',
+      ),
+    );
+  });
+
+  group('usePageController', () {
+    testWidgets('initial values matches with real constructor', (tester) async {
+      PageController controller;
+      PageController controller2;
+
+      await tester.pumpWidget(
+        HookBuilder(builder: (context) {
+          controller2 = PageController();
+          controller = usePageController();
+          return Container();
+        }),
+      );
+
+      expect(controller.initialPage, controller2.initialPage);
+      expect(controller.keepPage, controller2.keepPage);
+      expect(controller.viewportFraction, controller2.viewportFraction);
+    });
+    testWidgets("returns a PageController that doesn't change",
+        (tester) async {
+      PageController controller;
+      PageController controller2;
+
+      await tester.pumpWidget(
+        HookBuilder(builder: (context) {
+          controller = usePageController();
+          return Container();
+        }),
+      );
+
+      expect(controller, isA<PageController>());
+
+      await tester.pumpWidget(
+        HookBuilder(builder: (context) {
+          controller2 = usePageController();
+          return Container();
+        }),
+      );
+
+      expect(identical(controller, controller2), isTrue);
+    });
+
+    testWidgets('passes hook parameters to the PageController',
+        (tester) async {
+      PageController controller;
+
+      await tester.pumpWidget(
+        HookBuilder(
+          builder: (context) {
+            controller = usePageController(
+              initialPage: 42,
+              keepPage: false,
+              viewportFraction: 3.4,
+            );
+
+            return Container();
+          },
+        ),
+      );
+
+      expect(controller.initialPage, 42);
+      expect(controller.keepPage, false);
+      expect(controller.viewportFraction, 3.4);
+    });
+  });
+}
+
+class TickerProviderMock extends Mock implements TickerProvider {}
