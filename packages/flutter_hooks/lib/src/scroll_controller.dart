a5af5888c81b73efb2d5188391ef08f8283ae9fc
Frank Moreno <frankmoreno1993@gmail.com>
2021-04-23 10:17:36 -0500

    ading useScrollController.build()

--- packages/flutter_hooks/lib/src/scroll_controller.dart
+++ packages/flutter_hooks/lib/src/scroll_controller.dart
@@ -1,49 +1,56 @@
 part of 'hooks.dart';
 
+class _ScrollControllerCreator {
+  const _ScrollControllerCreator();
+
+  ScrollController call({
+    double initialScrollOffset = 0.0,
+    bool keepScrollOffset = true,
+    String? debugLabel,
+    List<Object?>? keys,
+  }) {
+    return use(
+      _ScrollControllerHook(
+        builder: () => ScrollController(
+          initialScrollOffset: initialScrollOffset,
+          keepScrollOffset: keepScrollOffset,
+          debugLabel: debugLabel,
+        ),
+        keys: keys,
+      ),
+    );
+  }
+
+  T build<T extends ScrollController>(
+    T Function() builder, {
+    List<Object?>? keys,
+  }) {
+    return use(_ScrollControllerHook(builder: builder, keys: keys)) as T;
+  }
+}
+
 /// Creates and disposes a [ScrollController].
 ///
 /// See also:
 /// - [ScrollController]
-ScrollController useScrollController({
-  double initialScrollOffset = 0.0,
-  bool keepScrollOffset = true,
-  String? debugLabel,
-  List<Object?>? keys,
-}) {
-  return use(
-    _ScrollControllerHook(
-      initialScrollOffset: initialScrollOffset,
-      keepScrollOffset: keepScrollOffset,
-      debugLabel: debugLabel,
-      keys: keys,
-    ),
-  );
-}
+///
+/// For custom scroll controllers (like [FixedExtentScrollController])
+///
+const useScrollController = _ScrollControllerCreator();
 
 class _ScrollControllerHook extends Hook<ScrollController> {
-  const _ScrollControllerHook({
-    required this.initialScrollOffset,
-    required this.keepScrollOffset,
-    this.debugLabel,
-    List<Object?>? keys,
-  }) : super(keys: keys);
+  const _ScrollControllerHook({required this.builder, List<Object?>? keys})
+      : super(keys: keys);
 
-  final double initialScrollOffset;
-  final bool keepScrollOffset;
-  final String? debugLabel;
+  final ScrollController Function() builder;
 
   @override
-  HookState<ScrollController, Hook<ScrollController>> createState() =>
-      _ScrollControllerHookState();
+  _ScrollControllerHookState createState() => _ScrollControllerHookState();
 }
 
 class _ScrollControllerHookState
     extends HookState<ScrollController, _ScrollControllerHook> {
-  late final controller = ScrollController(
-    initialScrollOffset: hook.initialScrollOffset,
-    keepScrollOffset: hook.keepScrollOffset,
-    debugLabel: hook.debugLabel,
-  );
+  late final ScrollController controller = hook.builder();
 
   @override
   ScrollController build(BuildContext context) => controller;
