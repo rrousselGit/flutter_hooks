ffa3188ecfa77d2c6b2fb1cb5e3d631d2365ddb9
Remi Rousselet <darky12s@gmail.com>
2021-05-13 08:50:56 +0100

    fix warnings

--- packages/flutter_hooks/test/memoized_test.dart
+++ packages/flutter_hooks/test/memoized_test.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/widgets.dart';
 import 'package:flutter/foundation.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 
 import 'mock.dart';
