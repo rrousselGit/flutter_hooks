8706b9ab53048fe8f8ed3134880767e2268cd19e
Sahil Sonawane <iamsahilsonawane@gmail.com>
2022-07-20 14:51:03 +0530

    feat: add focusScopeNode hook

---
+++ packages/flutter_hooks/lib/src/focus_scope_node.dart
@@ -0,0 +1,78 @@
+part of 'hooks.dart';
+
+/// Creates an automatically disposed [FocusScopeNode].
+///
+/// See also:
+/// - [FocusScopeNode]
+FocusScopeNode useFocusScopeNode({
+  String? debugLabel,
+  FocusOnKeyCallback? onKey,
+  FocusOnKeyEventCallback? onKeyEvent,
+  bool skipTraversal = false,
+  bool canRequestFocus = true,
+  bool descendantsAreFocusable = true,
+}) {
+  return use(
+    _FocusScopeNodeHook(
+      debugLabel: debugLabel,
+      onKey: onKey,
+      onKeyEvent: onKeyEvent,
+      skipTraversal: skipTraversal,
+      canRequestFocus: canRequestFocus,
+      descendantsAreFocusable: descendantsAreFocusable,
+    ),
+  );
+}
+
+class _FocusScopeNodeHook extends Hook<FocusScopeNode> {
+  const _FocusScopeNodeHook({
+    this.debugLabel,
+    this.onKey,
+    this.onKeyEvent,
+    required this.skipTraversal,
+    required this.canRequestFocus,
+    required this.descendantsAreFocusable,
+  });
+
+  final String? debugLabel;
+  final FocusOnKeyCallback? onKey;
+  final FocusOnKeyEventCallback? onKeyEvent;
+  final bool skipTraversal;
+  final bool canRequestFocus;
+  final bool descendantsAreFocusable;
+
+  @override
+  _FocusScopeNodeHookState createState() {
+    return _FocusScopeNodeHookState();
+  }
+}
+
+class _FocusScopeNodeHookState
+    extends HookState<FocusScopeNode, _FocusScopeNodeHook> {
+  late final FocusScopeNode _focusScopeNode = FocusScopeNode(
+    debugLabel: hook.debugLabel,
+    onKey: hook.onKey,
+    onKeyEvent: hook.onKeyEvent,
+    skipTraversal: hook.skipTraversal,
+    canRequestFocus: hook.canRequestFocus,
+  );
+
+  @override
+  void didUpdateHook(_FocusScopeNodeHook oldHook) {
+    _focusScopeNode
+      ..debugLabel = hook.debugLabel
+      ..skipTraversal = hook.skipTraversal
+      ..canRequestFocus = hook.canRequestFocus
+      ..onKey = hook.onKey
+      ..onKeyEvent = hook.onKeyEvent;
+  }
+
+  @override
+  FocusScopeNode build(BuildContext context) => _focusScopeNode;
+
+  @override
+  void dispose() => _focusScopeNode.dispose();
+
+  @override
+  String get debugLabel => 'useFocusScopeNode';
+}
