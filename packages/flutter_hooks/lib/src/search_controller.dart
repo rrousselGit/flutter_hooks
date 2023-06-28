11f247bf0a4c8eb888f3158924bfcaae6a193ac9
snapsl <snapsl@users.noreply.github.com>
2023-06-28 11:32:05 +0200

    Implement searchcontroller hook

---
+++ packages/flutter_hooks/lib/src/search_controller.dart
@@ -0,0 +1,33 @@
+part of 'hooks.dart';
+
+/// Creates a [SearchController] that will be disposed automatically.
+///
+/// See also:
+/// - [SearchController]
+SearchController useSearchController({List<Object?>? keys}) => use(
+      _SearchControllerHook(
+        keys: keys,
+      ),
+    );
+
+class _SearchControllerHook extends Hook<SearchController> {
+  const _SearchControllerHook({List<Object?>? keys}) : super(keys: keys);
+
+  @override
+  HookState<SearchController, Hook<SearchController>> createState() =>
+      _SearchControllerHookState();
+}
+
+class _SearchControllerHookState
+    extends HookState<SearchController, _SearchControllerHook> {
+  late final controller = SearchController();
+
+  @override
+  String get debugLabel => 'useSearchController';
+
+  @override
+  SearchController build(BuildContext context) => controller;
+
+  @override
+  void dispose() => controller.dispose();
+}
