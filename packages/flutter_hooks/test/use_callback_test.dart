3558a740af706b7e545010d1a08c56da3b04909f
Jaesung Hong <jaesunghong@Jaesungs-Mac-mini.local>
2023-07-11 00:12:08 +0900

    Move existing useCallback test to new file

--- packages/flutter_hooks/test/use_callback_test.dart
+++ packages/flutter_hooks/test/use_callback_test.dart
@@ -4,6 +4,42 @@ import 'package:flutter_hooks/flutter_hooks.dart';
 import 'mock.dart';
 
 void main() {
+  testWidgets('useCallback', (tester) async {
+    late int Function() fn;
+
+    await tester.pumpWidget(
+      HookBuilder(builder: (context) {
+        fn = useCallback<int Function()>(() => 42, []);
+        return Container();
+      }),
+    );
+
+    expect(fn(), 42);
+
+    late int Function() fn2;
+
+    await tester.pumpWidget(
+      HookBuilder(builder: (context) {
+        fn2 = useCallback<int Function()>(() => 42, []);
+        return Container();
+      }),
+    );
+
+    expect(fn2, fn);
+
+    late int Function() fn3;
+
+    await tester.pumpWidget(
+      HookBuilder(builder: (context) {
+        fn3 = useCallback<int Function()>(() => 21, [42]);
+        return Container();
+      }),
+    );
+
+    expect(fn3, isNot(fn));
+    expect(fn3(), 21);
+  });
+
   testWidgets(
     'should return same function when keys are not specified',
     (tester) async {
