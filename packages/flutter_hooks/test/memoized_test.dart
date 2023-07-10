ba32988f134ef0126be5852ce57ffabe53ba55b3
Jaesung Hong <jezsung@gmail.com>
2023-07-11 03:09:37 +0900

    Make useCallback hook's keys optional (#369)

--- packages/flutter_hooks/test/memoized_test.dart
+++ packages/flutter_hooks/test/memoized_test.dart
@@ -65,42 +65,6 @@ void main() {
         reason: 'The ref value has the last assigned value.');
   });
 
-  testWidgets('useCallback', (tester) async {
-    late int Function() fn;
-
-    await tester.pumpWidget(
-      HookBuilder(builder: (context) {
-        fn = useCallback<int Function()>(() => 42, []);
-        return Container();
-      }),
-    );
-
-    expect(fn(), 42);
-
-    late int Function() fn2;
-
-    await tester.pumpWidget(
-      HookBuilder(builder: (context) {
-        fn2 = useCallback<int Function()>(() => 42, []);
-        return Container();
-      }),
-    );
-
-    expect(fn2, fn);
-
-    late int Function() fn3;
-
-    await tester.pumpWidget(
-      HookBuilder(builder: (context) {
-        fn3 = useCallback<int Function()>(() => 21, [42]);
-        return Container();
-      }),
-    );
-
-    expect(fn3, isNot(fn));
-    expect(fn3(), 21);
-  });
-
   testWidgets('memoized without parameter calls valueBuilder once',
       (tester) async {
     late int result;
