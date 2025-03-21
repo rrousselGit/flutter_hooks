44d23b7760eb2badd63fdba380bb15f2e3687529
Bent Hillerkus <29630575+benthillerkus@users.noreply.github.com>
2025-03-22 00:53:32 +0100

    feat: add Hook for managing SnapshotController instances

---
+++ packages/flutter_hooks/lib/src/snapshot_controller.dart
@@ -0,0 +1,61 @@
+part of 'hooks.dart';
+
+/// Creates and disposes a [SnapshotController].
+/// 
+/// Note that [allowSnapshotting] must be set to `true`
+/// in order for this controller to actually do anything.
+/// This is consistent with [SnapshotController.new].
+/// 
+/// If [allowSnapshotting] changes on subsequent calls to [useSnapshotController],
+/// [SnapshotController.allowSnapshotting] will be called to update accordingly.
+/// 
+/// ```dart
+/// final controller = useSnapshotController(allowSnapshotting: true);
+/// // is equivalent to
+/// final controller = useSnapshotController();
+/// controller.allowSnapshotting = true;
+/// ```
+///
+/// See also:
+/// - [SnapshotController]
+SnapshotController useSnapshotController({
+  bool allowSnapshotting = false,
+}) {
+  return use(
+    _SnapshotControllerHook(
+      allowSnapshotting: allowSnapshotting,
+    ),
+  );
+}
+
+class _SnapshotControllerHook extends Hook<SnapshotController> {
+  const _SnapshotControllerHook({
+    required this.allowSnapshotting,
+  });
+
+  final bool allowSnapshotting;
+
+  @override
+  HookState<SnapshotController, Hook<SnapshotController>>
+      createState() => _SnapshotControllerHookState();
+}
+
+class _SnapshotControllerHookState
+    extends HookState<SnapshotController, _SnapshotControllerHook> {
+  late final controller = SnapshotController(allowSnapshotting: hook.allowSnapshotting);
+
+  @override
+  void didUpdateHook(_SnapshotControllerHook oldHook) {
+    super.didUpdateHook(oldHook);
+    controller.allowSnapshotting = hook.allowSnapshotting;
+  }
+
+  @override
+  SnapshotController build(BuildContext context) => controller;
+
+  @override
+  void dispose() => controller.dispose();
+
+  @override
+  String get debugLabel => 'useSnapshotController';
+}
